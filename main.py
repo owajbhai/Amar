78eb047cdc4600f22316a770d225a3d42351d70b
owajbhai <owajshaikhrcm@gmail.com>
2025-05-16 13:40:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -100232749407
+pyrogram.utils.MIN_CHANNEL_ID = -1002327494077
 
 if __name__ == "__main__":
     Bot().run()
