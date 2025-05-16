785c66c20d6865683b6d56209035a90799e75bc2
owajbhai <owajshaikhrcm@gmail.com>
2025-05-16 06:22:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ APP_ID = int(os.environ.get("APP_ID", "16708873")) #Your API ID from my.telegram
 API_HASH = os.environ.get("API_HASH", "c02a8adf652d357207e6fde153f83874") #Your API Hash from my.telegram.org
 #--------------------------------------------
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002600199074")) #Your db channel Id
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "")) #Your db channel Id
 OWNER = os.environ.get("OWNER", "ExE_AQUIV") # Owner username without @
 OWNER_ID = int(os.environ.get("OWNER_ID", "7638075280")) # Owner id
 #--------------------------------------------
