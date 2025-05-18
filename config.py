6ea301acd414b63f9bc884ee450b19fb64059c20
owajbhai <owajshaikhrcm@gmail.com>
2025-05-18 16:43:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -92,7 +92,7 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<b>ᴘᴏᴡᴇʀᴇᴅ ʙʏ:
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False #set True if you want to prevent users from forwarding files from bot
 #--------------------------------------------
 #Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", True) == 'True'
 #--------------------------------------------
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘᴀɪ!!"
